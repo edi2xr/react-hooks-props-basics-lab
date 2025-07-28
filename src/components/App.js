5d109e424137347f209b45b7d89db50d5e463c5e
edi2xr <mutuaadrian73@gmail.com>
2025-07-28 10:24:52 +0300

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
+// src/components/App.js
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
+import NavBar from "./NavBar"; // ✅ add this import
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar /> {/* ✅ add this line */}
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
